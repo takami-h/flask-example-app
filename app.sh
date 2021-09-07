362da290accbda38f111d44361f4b63ad5375037
Takami Hirata <thirata@redhat.com>
2021-09-07 17:32:49 +0900

    feat: add initial delay to use probes

--- app.sh
+++ app.sh
@@ -2,5 +2,7 @@
 # OpenShift S2I adapter script
 
 export FLASK_APP=flaskr
-flask init-db && flask run -h 0.0.0.0 -p ${PORT:-8080}
+
+sleep ${INITIAL_DELAY:-0} &&
+  flask init-db && flask run -h 0.0.0.0 -p ${PORT:-8080}
 
